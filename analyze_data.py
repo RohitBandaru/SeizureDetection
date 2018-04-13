fdac6591212513e2e08195b26f70d02577e0e0f3
Rohit Bandaru <rb696@cornell.edu>
2018-04-13 09:52:36 -0400

    data loading

---
+++ analyze_data.py
@@ -0,0 +1,24 @@
+import scipy.io as spio
+from scipy import signal
+import pywt
+import matplotlib.pyplot as plt
+import numpy as np
+
+path1 = "data/patient_2/ictal train/patient_2_121.mat"
+
+for i in range(1,2):
+	path = "data/patient_"+str(i)+"/ictal train/patient_"+str(i)+"_1.mat"
+	path_t = "data/patient_1/ictal train/patient_"+str(i)+"_1.mat"
+	data = spio.loadmat(path)["data"]
+	wp = pywt.WaveletPacket2D(data=data, wavelet='db1', mode='symmetric')
+
+	for i in range(0,wp.maxlevel+1):
+		print(np.log(np.sum(wp.get_level(i)[0].data.shape)))
+	'''
+	f, t, Sxx = signal.spectrogram(data[:,10], 5000)
+	print(Sxx/np.max(Sxx))
+	plt.pcolormesh(t, f, Sxx)
+	plt.show()
+	'''
+	#for node in wp.get_level(4):
+	#	print(node.data.shape)
