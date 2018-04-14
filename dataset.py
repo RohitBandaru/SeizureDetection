3a228555c6e37ce3e0662144320d6ad4c99837af
Rohit Bandaru <rb696@cornell.edu>
2018-04-14 16:56:14 -0400

    sklearn SVM

--- dataset.py
+++ dataset.py
@@ -3,7 +3,7 @@ from os import listdir
 import scipy.io as spio
 import numpy as np
 '''
-data: (n,m)
+data: (m,n)
 '''
 def directory_data(path):
 	data = None
@@ -14,7 +14,7 @@ def directory_data(path):
 		if data is None:
 			data = vec
 		else:
-			data = np.hstack([data, vec])
+			data = np.vstack([data, vec])
 	return data
 
 
@@ -25,23 +25,42 @@ def get_data(patient_number):
 	n_ictal = ictal_train.shape[1]
 	n_non_ictal = ictal_train.shape[1]
 
-	train_data = np.hstack([ictal_train, non_ictal_train])
-	labels = np.vstack([np.ones((n_ictal,1)),np.zeros((n_non_ictal,1))])
+	train_data = np.vstack([ictal_train, non_ictal_train])
+	labels = np.hstack([np.ones((n_ictal)),np.zeros((n_non_ictal))])
 
 	# shuffle data
 	rand_idx = np.random.permutation(labels.shape[0])
 
 	labels = labels[rand_idx]
-	train_data  = train_data[:,rand_idx]
+	train_data  = train_data[rand_idx,:]
 
 	return train_data, labels
 
+def train_val_split(data,labels,split):
+	m = data.shape[0]
+	train_m = int(m*split)
+	vali_m = m - train_m
 
+	train_data = data[:train_m,:]
+	train_labels = labels[:train_m]
 
-'''
+	val_data = data[train_m:,:]
+	val_labels = labels[train_m:]
+
+	return train_data, train_labels, val_data, val_labels
+
 data, labels = get_data(1)
 
-print(data.shape)
-print(labels)
-print(labels.shape)
-'''
+train_data, train_labels, val_data, val_labels = train_val_split(data, labels, .7)
+
+from sklearn.svm import SVC
+clf = SVC()
+clf.fit(train_data, train_labels)
+y_pred = clf.predict(val_data)
+
+from sklearn.metrics import accuracy_score
+print(y_pred)
+print(accuracy_score(y_pred, val_labels))
+
+
+
--- feature_extractor.py
+++ feature_extractor.py
@@ -3,4 +3,4 @@ import numpy as np
 data: (t,c)
 '''
 def extract_feature(data):
-	return np.random.randn(100,1)
+	return np.random.randn(1,100)
